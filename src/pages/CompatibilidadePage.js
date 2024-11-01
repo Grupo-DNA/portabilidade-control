cd14bfb5604c8e1ee21da5eee43263632004cb18
otaviopl <otavio.pereira.lopes@gmail.com>
2024-11-01 11:43:44 -0300

    add new config of compatibility value

--- src/pages/CompatibilidadePage.js
+++ src/pages/CompatibilidadePage.js
@@ -4,8 +4,9 @@ import './compatibilidade.css';
 import ScrollArrow from '../components/setaScroll';
 
 const CompatibilityPage = ({ compatibilityValue }) => {
-  const roundedCompatibilityValue = Math.round(compatibilityValue);
-  const compatibilityValue2cases = compatibilityValue.toFixed(2);
+  const numericCompatibilityValue = Number(compatibilityValue);
+  const roundedCompatibilityValue = Math.round(numericCompatibilityValue);
+  const compatibilityValue2cases = numericCompatibilityValue.toFixed(2);
 
   return (
     <div className="compatibility-container">
@@ -15,7 +16,7 @@ const CompatibilityPage = ({ compatibilityValue }) => {
         <h2>{roundedCompatibilityValue}%</h2> {/* H2 dentro da imagem */}
       </div>
       <div className='text-section-compatibility'>
-        <p>Seus dados genéticos têm <b>{compatibilityValue}% de compatibilidade</b> com as informações da <b>DNA Club.</b></p>
+        <p>Seus dados genéticos têm <b>{compatibilityValue2cases}% de compatibilidade</b> com as informações da <b>DNA Club.</b></p>
         <p>Seu <b>laudo já está sendo preparado</b> e você receberá um <b>e-mail com o acesso</b></p>
       </div>
       <ScrollArrow targetId="ecosystem-section" />
